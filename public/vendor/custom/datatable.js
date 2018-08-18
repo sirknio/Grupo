0c79da2a4aa9ec0421d073ea8810546b306fc2ce
Carlos Arboleda <knio16@gmail.com>
2018-08-18 15:33:35 -0500

    Cambio sobre BD y language sobre tablas

--- public/vendor/custom/datatable.js
+++ public/vendor/custom/datatable.js
@@ -5,9 +5,55 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+	$('#dataTables-integrantes').DataTable({
+		"lengthMenu": [ [4, 10, 25, 50, -1], [4, 10, 25, 50, "Todos"] ],
+		"language": {
+			"search": "Buscar",
+			"info": "Mostrando _TOTAL_ registros",
+			"lengthMenu": "Mostrar _MENU_ registros",
+			"infoFiltered": " - de _MAX_ total de registros",
+			"paginate": {
+				first:    '«',
+				previous: '‹',
+				next:     '›',
+				last:     '»'
+			},
+			aria: {
+				paginate: {
+					first:    'First',
+					previous: 'Previous',
+					next:     'Next',
+					last:     'Last'
+				}
+			}
+		  }		
+	});
+});
+
+$(document).ready(function() {
 	$('#dataTablesEvento').DataTable({
 		responsive: true,
-		//"pagingType": "full_numbers",
-		"order": [[ 0, "desc" ]]
+		"lengthMenu": [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "Todos"] ],
+		"order": [[ 0, "desc" ]],
+		"language": {
+			"search": "Buscar",
+			"info": "Total _TOTAL_ registros",
+			"lengthMenu": "Mostrar _MENU_ registros",
+			"infoFiltered": " - de _MAX_ total de registros",
+			"paginate": {
+				first:    '«',
+				previous: '‹',
+				next:     '›',
+				last:     '»'
+			},
+			aria: {
+				paginate: {
+					first:    'First',
+					previous: 'Previous',
+					next:     'Next',
+					last:     'Last'
+				}
+			}
+		  }		
 	});
 });

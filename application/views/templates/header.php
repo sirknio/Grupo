b37e1f9f50afb628e61d45a7451f5cf3cce70672
Carlos Arboleda <knio16@gmail.com>
2019-02-08 11:18:15 -0500

    Adapatacion logo como ico

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
 
     <title>Klipsia</title>
+    <link rel="icon" type="image/png" href="<?=base_url('')?>public/images/default/favicon.png" />
 
     <!-- Bootstrap Core CSS -->
     <link href="<?=base_url('')?>public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">

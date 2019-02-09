e7ee956010c18cae310177ca556b171e4b3c2ce9
Carlos Arboleda <knio16@gmail.com>
2019-02-09 07:56:44 -0500

    Mejoras Sesiones: Se remueve columna idPersona

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -22,7 +22,6 @@ class Login extends CI_Controller {
 					$this->session->set_userdata('Usuario',$user['Usuario']);
 					$this->session->set_userdata('Email',$user['Email']);
 					$this->session->set_userdata('TipoUsuario',$user['TipoUsuario']);
-					$this->session->set_userdata('idPersona',$user['idPersona']);
 					$this->session->set_userdata('idGrupo',$user['idGrupo']);
 					$this->session->set_userdata('Nombre',$user['Nombre']);
 					$this->session->set_userdata('Apellido',$user['Apellido']);
